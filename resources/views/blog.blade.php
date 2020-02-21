cf7c661fe48cf32524d74a7c816fc6797f6ea963
Satrio Utomo <szatrio@gmail.com>
2020-02-21 09:49:54 +0700

    Initial commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Tutorial Laravel - www.malasngoding.com</title>
+</head>
+<body>
+ 
+	<h3>Satrio Utomo</h3>
+	<p>Ini adalah percobaan dalam menggunkan Laravel</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+ 
+</body>
+</html>
---
+++ resources/views/employeelist.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel #4 : Passing Data Controller Ke View Laravel - www.malasngoding.com</title>
+</head>
+<body>
+ 
+	<h1>Tutorial Laravel</h1>
+	<a>Satrio Utomo</a>
+ 
+</body>
+</html>
